20089ea8eacc1c0da63b833fc80d072f690f9ebd
zrp21 <tehaji@126.com>
2024-09-18 11:09:29 -0400

    small fix

---
+++ checkpoints/sam1/download.sh
@@ -0,0 +1,3 @@
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
